ce8bf403950a7c1375d136e46536b546dccde7d9
jimmy <jim83531@gmail.com>
2022-05-06 10:31:01 +0000

    release for python>=3.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding="utf-8") as readme:
 
 setuptools.setup(
     name = 'modelbricks',
-    version='0.1.2',
+    version='0.1.3',
     author='Jimmy Su',
     author_email = 'jim83531@gmail.com',
     description = 'ML model brick',
@@ -21,7 +21,7 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     extras_require={
         "tensorflow": ['tensorflow >= 2.7.0'],
         "tensorflow-gpu": ['tnesorflow-gpu >= 2.7.0']
